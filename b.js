40d287668f08102e526fcd0007d2c2fcb6e88f75
Kinas <24854132+nanokina@users.noreply.github.com>
2020-02-24 18:27:24 +0900

    ミスった

--- b.js
+++
@@ -1,6 +0,0 @@
-
-const R = new RegExp('[\.\?\(\)\{\^]+', 'g');
-const SilentRegexp = new RegExp('[\s　,\.\?!\^\(\)`:\'"`;\{\}\\[\\]。、，．‥・…ー～~]+', 'g');
-const a = 'お^^^^ま[]()えな???～～～、、、、'.replace(SilentRegexp, '');
-
-console.log(a);
