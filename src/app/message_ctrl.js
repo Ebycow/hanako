8c918958534a39a73f9ff43ca94538066d575c86
Kinas <24854132+nanokina@users.noreply.github.com>
2020-03-17 18:23:44 +0900

    はなこ幼女化計画
    - 何もできない頃のはなこに戻ってもらう
    
    ロジック復活させるときはツリー参照する
    github.com/Ebycow/hanako/tree/d7de50d0fc62a3b68b5091a4f96ae99de4cda5e3

--- src/app/message_ctrl.js
+++ src/app/message_ctrl.js
@@ -1,14 +1,7 @@
 const path = require('path');
 const logger = require('log4js').getLogger(path.basename(__filename));
-const Injector = require('../core/injector');
 const MessageBuilder = require('../service/message_builder');
 const MessageService = require('../service/message_service');
-const { MessageContext } = require('../contexts/messagecontext');
-const { UrlReplacer } = require('../utils/replacer');
-const { AudioAdapterManager } = require('../adapters/audioadapter');
-const { FileAdapterErrors } = require('../adapters/fileadapter');
-const { ContentType } = require('../commands/commandresult');
-const IDiscordServerRepo = require('../domain/repos/i_discord_server_repo');
 
 /** @typedef {import('discord.js').Client} discord.Client */
 /** @typedef {import('discord.js').Message} discord.Message */
@@ -32,9 +25,6 @@ class MessageCtrl {
         this.client = client; // TODO 持ってないとだめ？
         this.servers = servers; // TODO FIX
 
-        // TODO FIX Repoはこの層(app)にDIされない
-        this.serverRepo = Injector.resolve(IDiscordServerRepo);
-
         this.client.on('message', message => this.onMessage(message).catch(handleUncaughtError));
     }
 
@@ -42,53 +32,7 @@ class MessageCtrl {
      * @param {discord.Message} message
      */
     async onMessage(message) {
-        // TODO FIX
-        if (!message.guild) return;
-        // TODO FIX
-        const key = message.guild.id;
-
-        // TODO FIX
-        const server = await this.serverRepo.loadOrCreate(key);
-
-        const voiceJoin = async () => {
-            await server.vc.join(message.member.voice.channel);
-            server.mainChannel = message.channel;
-            return `${message.channel}`;
-        };
-
-        const voiceLeave = () => {
-            server.vc.leave();
-            server.mainChannel = null;
-        };
-
-        const resolveUserName = userId => {
-            const member = message.mentions.members.find(m => userId === m.id);
-            if (member) {
-                return member.displayName;
-            } else {
-                // Discordタグ直打ちの場合
-                return '誰ですか？';
-            }
-        };
-
-        const context = new MessageContext({
-            isMainChannel: !!server.mainChannel && message.channel.id === server.mainChannel.id,
-            isAuthorInVC: !!message.member.voice.channel,
-            isJoined: () => server.vc.isJoined,
-            isSpeaking: () => server.vc.isStreaming || server.vc.queueLength > 0,
-            queueLength: () => server.vc.queueLength,
-            queuePurge: () => server.vc.clearQueue(),
-            voiceJoin,
-            voiceLeave,
-            voiceCancel: () => server.vc.killStream(),
-            authorId: message.author.id,
-            mentionedUsers: message.mentions.members.reduce((map, m) => map.set(m.displayName, m.id), new Map()),
-            resolveUserName,
-            resolveRoleName: x => message.mentions.roles.find(r => x === r.id).name,
-            resolveChannelName: x => message.mentions.channels.find(c => x === c.id).name,
-        });
-
-        const builder = new MessageBuilder(this.client, message, context);
+        const builder = new MessageBuilder(this.client, message, null);
         const dmessage = await builder.build({
             id: message.id,
             content: message.content,
@@ -108,63 +52,6 @@ class MessageCtrl {
         const service = new MessageService();
         const tmp = await service.serve(dmessage);
         logger.info(tmp);
-
-        // TODO FIX とりあえず動かすためにやっている
-        message.content = dmessage.content;
-
-        if (server.isCommandMessage(message)) {
-            try {
-                const result = await server.handleMessage(context, message);
-                if (result.replyText) {
-                    const sentMessage = await message.channel.send(result.replyText);
-                    if (result.contentType === ContentType.PAGER) {
-                        await sentMessage.react('👈');
-                        await sentMessage.react('👉');
-                    }
-                }
-            } catch (err) {
-                logger.error('コマンド処理でエラー', err);
-                return;
-            }
-        } else if (server.isMessageToReadOut(message)) {
-            let text = message.content;
-
-            // URL置換
-            text = UrlReplacer.replace(text);
-
-            // リプレーサーによる置換
-            text = server.handleReplace(context, text);
-
-            logger.trace('リクエスト直前のtext:', text);
-
-            // リクエストコンバーターによる変換
-            const requests = server.createRequests(context, text);
-
-            logger.trace(requests);
-
-            // リクエストの実行
-            let stream;
-            try {
-                stream = await AudioAdapterManager.request(...requests);
-            } catch (err) {
-                if (err === FileAdapterErrors.NOT_FOUND) {
-                    logger.warn('リクエストしたファイルが見つからなかった。', requests);
-                    return;
-                } else {
-                    logger.error('オーディオリクエストでエラー', err);
-                    return;
-                }
-            }
-
-            // awaitが絡んだのでここではnullの可能性があるよ
-            if (!server.vc.isJoined) {
-                logger.info('オーディオリクエスト中にVC切断されてました。', message.guild.name);
-                stream.destroy();
-                return;
-            }
-
-            server.vc.push(stream);
-        }
     }
 }
 
