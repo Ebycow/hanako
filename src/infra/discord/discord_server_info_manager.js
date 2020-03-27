dcebff290f7cf63a5929b0eece2fc8aabfcb399f
Kinas <24854132+nanokina@users.noreply.github.com>
2020-03-28 02:16:24 +0900

    IServerStatusRepoにDiscord境界から実装を付与

--- src/infra/discord/discord_server_info_manager.js
+++ src/infra/discord/discord_server_info_manager.js
@@ -1,62 +1,53 @@
 const path = require('path');
 const logger = require('log4js').getLogger(path.basename(__filename));
 const assert = require('assert').strict;
+const uuid = require('uuidv4').uuid;
 const discord = require('discord.js');
 const errors = require('../../core/errors').promises;
 const Injector = require('../../core/injector');
-const IDiscordServerRepo = require('../../domain/repo/i_discord_server_repo');
-const IWordDictionaryRepo = require('../../domain/repo/i_word_dictionary_repo');
-const DiscordServer = require('../../domain/model/discord_server');
+const IServerStatusRepo = require('../../domain/repo/i_server_status_repo');
 const ServerStatus = require('../../domain/entity/server_status');
-const DiscordVoiceQueueManager = require('./discord_voice_queue_manager');
 
 // unused
 logger;
 
-// TODO FIX モデルのコンストラクトはドメインサービス化すべき
-
 class DiscordServerInfoManager {
     /**
+     * DIコンテナ用コンストラクタ
+     *
      * @param {null} client DI
-     * @param {null} wordRepo DI
      */
-    constructor(client = null, wordRepo = null) {
+    constructor(client = null) {
         this.client = client || Injector.resolveSingleton(discord.Client);
-        this.wordRepo = wordRepo || Injector.resolve(IWordDictionaryRepo);
-        this.voiceManager = new DiscordVoiceQueueManager();
     }
 
-    async load(id) {
-        assert(typeof id === 'string');
+    /**
+     * (impl) IServerStatusRepo
+     *
+     * @param {string} serverId
+     * @returns {Promise<ServerStatus>}
+     */
+    async loadServerStatus(serverId) {
+        assert(typeof serverId === 'string');
 
-        const G = this.client.guilds.resolve(id);
-        if (!G) {
-            errors.unexpected(`no-such-guild ${id}`);
+        const guild = this.client.guilds.resolve(serverId);
+        if (!guild) {
+            return errors.unexpected(`no-such-guild ${serverId}`);
         }
 
-        const wordDictionary = await this.wordRepo.loadWordDictionary(G.id);
+        // 新規サーバーステータスを生成
+        const serverStatus = new ServerStatus({
+            id: uuid(),
+            serverId: guild.id,
+            serverName: guild.name,
+        });
 
-        const blueprint = {
-            serverId: G.id,
-            serverName: G.name,
-            voiceStatus: null,
-            voiceChannel: null,
-            readingChannels: [],
-            wordDictionary,
-        };
-
-        const vc = this.voiceManager.getVoiceChatModel(id);
-        if (vc && vc.connection) {
-            blueprint.readingChannels = vc.readingChannels.map(c => c.id);
-            blueprint.voiceChannel = vc.connection.channel.id;
-            blueprint.voiceStatus = vc.dispatcher === null ? 'ready' : 'speaking';
-        }
-
-        const server = new DiscordServer(G.id, new ServerStatus(blueprint));
-        return Promise.resolve(server);
+        // サーバーステータスを返却
+        return Promise.resolve(serverStatus);
     }
 }
 
-IDiscordServerRepo.comprise(DiscordServerInfoManager);
+// IServerStatusRepoの実装として登録
+IServerStatusRepo.comprise(DiscordServerInfoManager);
 
 module.exports = DiscordServerInfoManager;
