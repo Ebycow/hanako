42313b189ef5e088d3b410c0d643cad84d8e6316
Kinas <24854132+nanokina@users.noreply.github.com>
2020-03-29 22:36:31 +0900

    HanakoLoaderに沈黙ユーザー辞書のロード処理を追加

--- src/service/hanako_loader.js
+++ src/service/hanako_loader.js
@@ -4,6 +4,7 @@ const ISettingsRepo = require('../domain/repo/i_settings_repo');
 const IServerStatusRepo = require('../domain/repo/i_server_status_repo');
 const IVoiceStatusRepo = require('../domain/repo/i_voice_status_repo');
 const IWordDictionaryRepo = require('../domain/repo/i_word_dictionary_repo');
+const ISilenceDictionaryRepo = require('../domain/repo/i_silence_dictionary_repo');
 const Hanako = require('../domain/model/hanako');
 
 /**
@@ -16,12 +17,20 @@ class HanakoLoader {
      * @param {null} serverStatusRepo DI
      * @param {null} voiceStatusRepo DI
      * @param {null} wordDictRepo DI
+     * @param {null} silenceDictRepo DI
      */
-    constructor(settingsRepo = null, serverStatusRepo = null, voiceStatusRepo = null, wordDictRepo = null) {
+    constructor(
+        settingsRepo = null,
+        serverStatusRepo = null,
+        voiceStatusRepo = null,
+        wordDictRepo = null,
+        silenceDictRepo = null
+    ) {
         this.settingsRepo = settingsRepo || Injector.resolve(ISettingsRepo);
         this.serverStatusRepo = serverStatusRepo || Injector.resolve(IServerStatusRepo);
         this.voiceStatusRepo = voiceStatusRepo || Injector.resolve(IVoiceStatusRepo);
         this.wordDictRepo = wordDictRepo || Injector.resolve(IWordDictionaryRepo);
+        this.silenceDictRepo = silenceDictRepo || Injector.resolve(ISilenceDictionaryRepo);
     }
 
     /**
@@ -38,9 +47,10 @@ class HanakoLoader {
         const serverStatus = await this.serverStatusRepo.loadServerStatus(serverId);
         const voiceStatus = await this.voiceStatusRepo.loadVoiceStatus(serverId);
         const wordDictionary = await this.wordDictRepo.loadWordDictionary(serverId);
+        const silenceDictionary = await this.silenceDictRepo.loadSilenceDictionary(serverId);
 
         // 花子モデルを生成して返却
-        const hanako = new Hanako(settings, serverStatus, voiceStatus, wordDictionary);
+        const hanako = new Hanako(settings, serverStatus, voiceStatus, wordDictionary, silenceDictionary);
         return Promise.resolve(hanako);
     }
 }
